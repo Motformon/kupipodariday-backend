29d96d6b8678ffd5455ee9bb6ee2224ce3dfb047
Михаил Петров <m.petrov@elcode.ru>
2024-03-07 16:28:03 +0300

    add methods user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/update-user.dto';
+import { UpdateUserDto } from './dto/create-user.dto';
+import { User } from './user.entity';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Post()
+  create(@Body() user: CreateUserDto) {
+    return this.usersService.create(user);
+  }
+
+  @Patch(':id')
+  async updateById(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() user: UpdateUserDto,
+  ) {
+    const findUser = await this.usersService.findById(id);
+    if (!findUser) {
+      throw new NotFoundException();
+    }
+    await this.usersService.updateById(id, user);
+  }
+
+  @Delete(':id')
+  async removeById(@Param('id', ParseIntPipe) id: number) {
+    const user = await this.usersService.findById(id);
+    if (!user) {
+      throw new NotFoundException();
+    }
+    await this.usersService.removeById(id);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { UpdateUserDto } from './dto/create-user.dto';
+import { CreateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  findById(id: number): Promise<User> {
+    return this.userRepository.findOneBy({ id });
+  }
+
+  removeById(id: number) {
+    return this.userRepository.delete({ id });
+  }
+
+  updateById(id: number, updateStudentDto: UpdateUserDto) {
+    return this.userRepository.update({ id }, updateStudentDto);
+  }
+
+  create(createStudentDto: CreateUserDto): Promise<User> {
+    return this.userRepository.save(createStudentDto);
+  }
+
+}
